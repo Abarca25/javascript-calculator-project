b7864f9ae8509b79dda9ab81621d6d376a83e0ba
Abarca25 <190577751+Abarca25@users.noreply.github.com.>
2024-12-25 23:29:05 -0800

    Added calculation logic

---
+++ script.js
@@ -0,0 +1,63 @@
+const numberButtons = document.querySelectorAll("#number");
+
+function add(arr) {
+    return arr.reduce((previousNumber, nextNumber) => previousNumber + nextNumber);
+}
+
+function subtract(arr) {
+    return arr.reduce((previousNumber, nextNumber) => previousNumber - nextNumber);
+}
+
+function multiply(arr) {
+    return arr.reduce((previousNumber, nextNumber) => previousNumber * nextNumber);
+}
+
+function divide(arr) {
+    return arr.reduce((previousNumber, nextNumber) => previousNumber / nextNumber);
+}
+
+function operate(operator, ...args) {
+    return operator(args);
+}
+
+function updateResultsBar(result) {
+    const resultsBar = document.querySelector("#result-bar");
+    resultsBar.textContent = result;
+}
+
+const calculatorButtons = document.querySelector(".button-layout");
+
+let currentNumber = '';
+let nextNumber = '';
+let mathToDisplay = '';
+let firstNumberSelected = false;
+let equationType = '';
+
+calculatorButtons.addEventListener("click", (e) => {
+    const button = e.target;
+
+    if (button.id === "number") {
+        if (!firstNumberSelected) {
+            currentNumber += button.innerText;
+            mathToDisplay += button.innerText;
+            updateResultsBar(mathToDisplay);
+        } else {
+            nextNumber += button.innerText;
+            mathToDisplay += button.innerText;
+            updateResultsBar(mathToDisplay);
+        }
+    } else if (button.id === "add" || button.id === "subtract" || button.id === "multiply" || button.id === "divide") {
+        if (firstNumberSelected && nextNumber !== '') {
+            currentNumber = operate(window[equationType], Number(currentNumber), Number(nextNumber));
+            mathToDisplay = currentNumber.toString();
+            nextNumber = '';
+            updateResultsBar(mathToDisplay);
+        }
+
+
+        firstNumberSelected = true;
+        equationType = button.id;
+        mathToDisplay += ` ${button.innerText} `;
+        updateResultsBar(mathToDisplay);
+    }
+});
